b040973ceb762be565ce70a5f304cca913c8546d
SunLightJuly <ygqy8181@qq.com>
2014-08-05 18:51:51 +0800

    modified LuaStack::executeScriptFile()

--- quick/lib/lua_bindings/manual/CCLuaStack.cpp
+++ quick/lib/lua_bindings/manual/CCLuaStack.cpp
@@ -283,27 +283,19 @@ int LuaStack::executeString(const char *codes)
 
 int LuaStack::executeScriptFile(const char* filename)
 {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
-    std::string code("require \"");
-    code.append(filename);
-    code.append("\"");
-    return executeString(code.c_str());
-#else
+    CCAssert(filename, "CCLuaStack::executeScriptFile() - invalid filename");
+
     std::string fullPath = FileUtils::getInstance()->fullPathForFilename(filename);
-    ++_callFromLua;
-    int nRet = luaL_dofile(_state, fullPath.c_str());
-    --_callFromLua;
-    CC_ASSERT(_callFromLua >= 0);
-    // lua_gc(_state, LUA_GCCOLLECT, 0);
-    
-    if (nRet != 0)
-    {
-        CCLOG("[LUA ERROR] %s", lua_tostring(_state, -1));
-        lua_pop(_state, 1);
-        return nRet;
+    ssize_t chunkSize = 0;
+    unsigned char *chunk = FileUtils::getInstance()->getFileData(fullPath.c_str(), "rb", &chunkSize);
+    int rn = 0;
+    if (chunk) {
+        if (luaLoadBuffer(_state, (const char*)chunk, (int)chunkSize, fullPath.c_str()) == 0) {
+            rn = executeFunction(0);
+        }
+        delete chunk;
     }
-    return 0;
-#endif
+    return rn;
 }
 
 int LuaStack::executeGlobalFunction(const char* functionName)
