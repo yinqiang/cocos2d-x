52efc051ac32608ab0255cfded74498ce8fe7ea6
htl <htlxyz@126.com>
2014-08-06 18:14:39 +0800

    modify ccsloader

--- quick/framework/cc/uiloader/init.lua
+++ quick/framework/cc/uiloader/init.lua
@@ -19,10 +19,56 @@ function uiloader:seekNodeByTag(parent, tag)
 	end
 
 	local findNode
-	for i,v in ipairs(parent:getChildren()) do
-		findNode = self:seekNodeByTag(v, tag)
-		if findNode then
-			return findNode
+	local children = parent:getChildren()
+	local childCount = parent:getChildrenCount()
+	if childCount < 1 then
+		return
+	end
+	for i=1, childCount do
+		if "table" == type(children) then
+			parent = children[i]
+		elseif "userdata" == type(children) then
+			parent = children:objectAtIndex(i - 1)
+		end
+
+		if parent then
+			findNode = self:seekNodeByTag(parent, tag)
+			if findNode then
+				return findNode
+			end
+		end
+	end
+
+	return
+end
+
+function uiloader:seekNodeByName(parent, name)
+	if not parent then
+		return
+	end
+
+	if name == parent.name then
+		return parent
+	end
+
+	local findNode
+	local children = parent:getChildren()
+	local childCount = parent:getChildrenCount()
+	if childCount < 1 then
+		return
+	end
+	for i=1, childCount do
+		if "table" == type(children) then
+			parent = children[i]
+		elseif "userdata" == type(children) then
+			parent = children:objectAtIndex(i - 1)
+		end
+
+		if parent then
+			findNode = self:seekNodeByName(parent, name)
+			if findNode then
+				return findNode
+			end
 		end
 	end
 
