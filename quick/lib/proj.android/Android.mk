1ce9793ca4ecd590319f54104104b0de282e39a5
SunLightJuly <ygqy8181@qq.com>
2014-08-07 15:43:19 +0800

    added lib/proj.android

---
+++ quick/lib/proj.android/Android.mk
@@ -0,0 +1,15 @@
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := quickcocos2dx
+LOCAL_MODULE_FILENAME := libquickcocos2dx
+
+LOCAL_CFLAGS := -Wno-psabi -DUSE_FILE32API -DCC_LUA_ENGINE_ENABLED=1 $(ANDROID_COCOS2D_BUILD_FLAGS)
+LOCAL_EXPORT_CFLAGS := -Wno-psabi -DUSE_FILE32API -DCC_LUA_ENGINE_ENABLED=1
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos_lua_static
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,lua_bindings)
