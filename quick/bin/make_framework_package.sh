15c8ffaa10ada14dc422f079f1127ad9373c7ff7
yinjimmy <jimmy.yin5@gmail.com>
2014-08-08 18:37:19 +0800

    move

--- quick/bin/make_framework_package.sh
+++ quick/bin/make_framework_package.sh
@@ -8,10 +8,10 @@ echo "### UPDATING ###"
 echo ""
 echo "updating all framework_precompiled.zip"
 echo ""
-echo templates/PROJECT_TEMPLATE_01/res/framework_precompiled.zip
-cp lib/framework_precompiled/framework_precompiled.zip templates/PROJECT_TEMPLATE_01/res/
+# echo templates/PROJECT_TEMPLATE_01/res/framework_precompiled.zip
+# cp lib/framework_precompiled/framework_precompiled.zip templates/PROJECT_TEMPLATE_01/res/
 
-for dest in `find samples -type f | grep "/res/framework_precompiled.zip"`
+for dest in `find samples templates -type f | grep "/res/framework_precompiled.zip"`
 do
     echo $dest
     cp lib/framework_precompiled/framework_precompiled.zip $dest
@@ -22,10 +22,8 @@ done
 echo ""
 echo "updating all framework_precompiled_wp8.zip"
 echo ""
-echo templates/PROJECT_TEMPLATE_01/res/framework_precompiled_wp8.zip
-cp lib/framework_precompiled/framework_precompiled_wp8.zip templates/PROJECT_TEMPLATE_01/res/
 
-for dest in `find samples -type f | grep "/res/framework_precompiled_wp8.zip"`
+for dest in `find samples templates -type f | grep "/res/framework_precompiled_wp8.zip"`
 do
     echo $dest
     cp lib/framework_precompiled/framework_precompiled_wp8.zip $dest
