44fc5999e3c62fcb2849fb7b83cc42cdf0eafec4
SunLightJuly <ygqy8181@qq.com>
2014-08-12 09:50:29 +0800

    restored LabelBMFont

--- cocos/cocos2d.h
+++ cocos/cocos2d.h
@@ -112,6 +112,7 @@ THE SOFTWARE.
 #include "2d/CCDrawNode.h"
 #include "2d/CCLabelAtlas.h"
 #include "2d/CCLabelTTF.h"
+#include "2d/CCLabelBMFont.h"
 #include "2d/CCLabel.h"
 #include "2d/CCFontFNT.h"
 #include "2d/CCLayer.h"
