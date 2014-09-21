b680c11c3ddf743eb031b341cf3538f7febb57b2
dualface <dualface@gmail.com>
2014-09-22 05:10:22 +0800

    update setup_win.bat

--- setup_win.bat
+++ setup_win.bat
@@ -1,12 +1,6 @@
 @echo off
 set QUICK_V3_ROOT=%~dp0
-
 set SETX=setx
-ver | find "XP" > nul
-if %ERRORLEVEL% == 0 (
-    set SETX=%QUICK_V3_ROOT%\bin\win32\setx.exe
-)
-
 
 echo.
 echo config:
@@ -19,4 +13,3 @@ echo.
 echo.
 
 pause
-
