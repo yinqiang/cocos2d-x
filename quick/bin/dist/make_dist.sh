ed7cc5e13359f02a00a7b668928ab6d2d19d8c53
dualface <dualface@gmail.com>
2014-09-24 15:40:49 +0800

    update make_dist.sh

--- quick/bin/dist/make_dist.sh
+++ quick/bin/dist/make_dist.sh
@@ -15,44 +15,64 @@ echo "VERSION = \"$VERSION\""
 echo "WORKDIR = \"$WORKDIR\""
 echo ""
 
-if [ -d "$WORKDIR" ]; then
-    rm -fr "$WORKDIR"
-fi
-mkdir -p "$WORKDIR"
+# if [ -d "$WORKDIR" ]; then
+#     rm -fr "$WORKDIR"
+# fi
+# mkdir -p "$WORKDIR"
 
-cd "$QUICK_V3_ROOT"
-git archive v3quick | tar -x -C "$WORKDIR"
+# cd "$QUICK_V3_ROOT"
+# git archive v3quick | tar -x -C "$WORKDIR"
 
-cd quick/player/proj.mac
+# cd quick/player/proj.mac
 
-cp -rf player3.xcodeproj/xcuserdata/USER.xcuserdatad/ player3.xcodeproj/xcuserdata/$USER.xcuserdatad/
-xcodebuild -configuration Debug \
-    CODE_SIGN_IDENTITY="Developer ID Application: $CERT" \
-    QUICK_V3_ROOT="$QUICK_V3_ROOT" \
-    -workspace player3.xcworkspace \
-    -scheme player3 \
-    -archivePath ./build \
-    archive
+# cp -rf player3.xcodeproj/xcuserdata/USER.xcuserdatad/ player3.xcodeproj/xcuserdata/$USER.xcuserdatad/
+# xcodebuild -configuration Debug \
+#     CODE_SIGN_IDENTITY="Developer ID Application: $CERT" \
+#     QUICK_V3_ROOT="$QUICK_V3_ROOT" \
+#     -workspace player3.xcworkspace \
+#     -scheme player3 \
+#     -archivePath ./build \
+#     archive
 
-mv ./build.xcarchive/Products/Applications/player3.app "$WORKDIR"
-rm -fr ./build.xcarchive
+# mv ./build.xcarchive/Products/Applications/player3.app "$WORKDIR"
+# rm -fr ./build.xcarchive
 
-spctl -a -v "$WORKDIR/player3.app/"
-echo ""
-echo "<QUICK>/player3.app/: accepted"
-echo "source=Developer ID"
-echo "override=security disabled"
-echo ""
+# spctl -a -v "$WORKDIR/player3.app/"
+# echo ""
+# echo "player3.app accepted"
+# echo "source=Developer ID"
+# echo "override=security disabled"
+# echo ""
 
 rm -fr $QUICK_V3_ROOT/quick/bin/dist/build/
 packagesbuild -v $QUICK_V3_ROOT/quick/bin/dist/quickcocos2dx.pkgproj
 
 du -h -d 1 $QUICK_V3_ROOT/quick/bin/dist/build
 echo ""
+echo "--------------------------"
+echo ""
 
 SRC_PKG=$QUICK_V3_ROOT/quick/bin/dist/build/quick-cocos2d-x-$VERSION.mpkg
 DEST_PKG=$QUICK_V3_ROOT/quick/bin/dist/build/Install.mpkg
-sudo productsign --sign "Developer ID Installer: $CERT" "$SRC_PKG/Contents/Packages/quickcocos2dx.pkg" "$DEST_PKG/Contents/Packages/quickcocos2dx.pkg"
+if [ -d "$DEST_PKG" ]; then
+	rm -fr "$DEST_PKG"
+fi
+if [ -f "$SRC_PKG/Contents/Packages/quickcocos2dx_signed.pkg" ]; then
+	rm -f "$SRC_PKG/Contents/Packages/quickcocos2dx_signed.pkg"
+fi
+
+productsign --sign "Developer ID Installer: $CERT" "$SRC_PKG/Contents/Packages/quickcocos2dx.pkg" "$SRC_PKG/Contents/Packages/quickcocos2dx_signed.pkg"
 echo ""
-sudo productsign --sign "Developer ID Application: $CERT" "$SRC_PKG" "$DEST_PKG"
+rm "$SRC_PKG/Contents/Packages/quickcocos2dx.pkg"
+mv "$SRC_PKG/Contents/Packages/quickcocos2dx_signed.pkg" "$SRC_PKG/Contents/Packages/quickcocos2dx.pkg"
+spctl -a -v --type install "$SRC_PKG/Contents/Packages/quickcocos2dx.pkg"
+echo ""
+echo "quickcocos2dx.pkg accepted"
+echo "source=Developer ID"
+echo "override=security disabled"
+echo ""
+echo "--------------------------"
+echo ""
+
+productsign --sign "Developer ID Application: $CERT" "$SRC_PKG" "$DEST_PKG"
 echo ""
